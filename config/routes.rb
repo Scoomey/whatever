efac18e67ae7fa032cdbb11aa2c83b348ea4414b
Stuart Coombe <s_coombe89@hotmail.co.uk>
2021-08-31 14:33:42 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :movies, only: [:show]
+
+  resources :foods, only: [:show]
+
+  resources :pairings, only: [:create, new, :show, :index]
 end

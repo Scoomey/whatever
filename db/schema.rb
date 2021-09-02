8037dabaa1ead1249688c93b1c0d36a2c27d9bdc
mohamed abdillahi <mohamed246@live.com>
2021-09-02 11:54:27 +0100

    fetching

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_08_31_170604) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_08_31_170604) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.integer "year"
+    t.string "genre"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "year"

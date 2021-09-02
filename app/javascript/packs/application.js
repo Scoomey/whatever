aebc114612e0d6af5bf2c15eef413ed353e5f0fb
Elise Burridge <emjburridge@gmail.com>
2021-09-02 14:40:47 +0100

    eventually figured out slider javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,13 +24,14 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import "fetch.js"
 import { searchMovies } from "./fetch";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { slider, displayValue } from "./slider";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-
+  slider();
+  displayValue();
 });

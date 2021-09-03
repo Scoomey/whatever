58f104f89930706224e4beada66c91a706f2bc38
Stuart Coombe <s_coombe89@hotmail.co.uk>
2021-09-03 12:46:46 +0100

    fixed dependant issues

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
 
-  has_many :pairing
-  has_many :movie_genres
+  has_many :pairing, dependent: :destroy
+  has_many :movie_genres, dependent: :destroy
   has_many :genres, through: :movie_genres
 
 end
